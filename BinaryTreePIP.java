2ee6a43d57cde1dc66b32b669c9499fa3f4a21ac
wasimasultana230 <wasimasultana230@gmail.com>
2025-07-15 23:31:39 +0600

    Count of Nodes

--- BinaryTreePIP.java
+++ BinaryTreePIP.java
@@ -92,6 +92,16 @@ public class BinaryTreePIP {
         }
     }
 
+    public static int countOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+        return leftNodes + rightNodes + 1;
+
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
@@ -103,6 +113,7 @@ public class BinaryTreePIP {
         postorder(root);
         System.out.println();
         levelorder(root);
+        System.out.println(countOfNodes(root));
 
     }
 }
