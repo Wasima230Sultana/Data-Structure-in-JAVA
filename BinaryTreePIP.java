9f2043cfe5f3ba2da9b5da6f7150b3570c82bd6c
wasimasultana230 <wasimasultana230@gmail.com>
2025-07-15 23:21:05 +0600

    Levelorder Traversal

--- BinaryTreePIP.java
+++ BinaryTreePIP.java
@@ -1,4 +1,7 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreePIP {
 
     static class Node {
@@ -61,6 +64,34 @@ public class BinaryTreePIP {
 
     }
 
+    public static void levelorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
@@ -70,6 +101,8 @@ public class BinaryTreePIP {
         inorder(root);
         System.out.println();
         postorder(root);
+        System.out.println();
+        levelorder(root);
 
     }
 }
