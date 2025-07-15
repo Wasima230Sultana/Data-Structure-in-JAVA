7b7decf6eb32fba00244c4df61d1209654971259
wasimasultana230 <wasimasultana230@gmail.com>
2025-07-15 22:57:50 +0600

    Inorder traversal

--- BinaryTreePIP.java
+++ BinaryTreePIP.java
@@ -41,11 +41,24 @@ public class BinaryTreePIP {
 
     }
 
+     public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+
+    }
+
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildtree(nodes);
         preorder(root);
+        System.out.println();
+        inorder(root);
 
     }
 }
