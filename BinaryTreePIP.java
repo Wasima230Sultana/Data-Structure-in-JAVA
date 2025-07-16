fac9ea645af080fcd22d277c1a859ec98c580ce6
wasimasultana230 <wasimasultana230@gmail.com>
2025-07-16 20:18:12 +0600

    Add Diameter of a tree

--- BinaryTreePIP.java
+++ BinaryTreePIP.java
@@ -116,15 +116,44 @@ public class BinaryTreePIP {
         return myHeight;
     }
 
-        public static int diameter(Node root) {
+    public static int diameter(Node root) {
         if (root == null) {
             return 0;
         }
         int diam1 = diameter(root.left);
         int diam2 = diameter(root.right);
-        int diam3 = height(root.left) + height(root.right) +1 ;
-        return Math.max(diam3 ,Math.max(diam1, diam2));
+        int diam3 = height(root.left) + height(root.right) + 1;
+        return Math.max(diam3, Math.max(diam1, diam2));
     }
+
+    static class TreeInfo {
+
+        int ht;
+        int diam;
+
+        TreeInfo(int ht, int diam) {
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+
+    public static TreeInfo diameter2(Node root) {
+        if (root == null) {
+            return new TreeInfo(0, 0);
+        }
+        TreeInfo left = diameter2(root.left);
+        TreeInfo right = diameter2(root.right);
+        int myHeight = Math.max(left.ht, right.ht) + 1;
+
+        int diam1 = left.diam;
+        int diam2 = right.diam;
+        int diam3 = left.ht + right.ht + 1;
+        int mydiam = Math.max(Math.max(diam1, diam2), diam3);
+
+        TreeInfo myInfo = new TreeInfo(myHeight, mydiam);
+        return myInfo;
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
@@ -145,6 +174,8 @@ public class BinaryTreePIP {
         System.out.println(height(root));
         // O(n^2)
         System.out.println(diameter(root));
+        // O(n)
+        System.out.println(diameter2(root).diam);
 
     }
 }
